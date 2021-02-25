66c262a7284a7fdb84002b3a75578467852961b4
Lukas Pohlman <pohluk@odooutv12.vertel.se>
2021-02-25 15:00:39 +0000

    odoo-membership:membership_invoice_fortnox. Removed package product from SFM invoice

--- membership_invoice_fortnox/models/account_invoice.py
+++ membership_invoice_fortnox/models/account_invoice.py
@@ -13,6 +13,21 @@ import json
 import logging
 _logger = logging.getLogger(__name__)
 
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+    
+    def remove_free_lines(self):
+        """ SFM does not want package products to show on the invoice. Call this function to remove them.
+        """
+        #_logger.warn("~ Got invoice %s with %s lines!" % (self.id, len(self.invoice_line_ids)))
+        self.state = 'draft'
+        for line in self.invoice_line_ids:
+            #_logger.warn("~ the line named %s has %s products" % (line.display_name, len(line.product_id.membership_product_ids)))
+            if len(line.product_id.membership_product_ids) > 0:
+                #_logger.warn("~ removing %s with id %s from invoice" % (line.display_name, line.id))
+                line.unlink()
+        self.state = 'open'
+
 class AccountInvoiceSend(models.TransientModel):
     _inherit = 'account.invoice.send'
 
@@ -25,6 +40,7 @@ class AccountInvoiceSend(models.TransientModel):
             for invoice in self.invoice_ids:
                 time.sleep(1)
                 if not invoice.name:
+                    invoice.remove_free_lines()
                     invoice.fortnox_create()
         return res
 
