1fe6c89d028664c29df1c1205f3739245b0f952c
Haze Lidman <haze.lidman@vertel.se>
2021-03-05 09:38:07 +0000

    Added function för ansvärförsäkring, but it is not working as it should

--- membership_insurance_days/models/insurance_days.py
+++ membership_insurance_days/models/insurance_days.py
@@ -24,12 +24,12 @@ class AccountInvoice(models.Model):
         
         return days
 
-    @api.onchange('partner_id')
+    @api.onchange('partner_id','price_unit','price_subtotal')
     def _check_dates_insurance(self):
         _logger.warn("~ Haze: in _check_dates_insurance")
         
         if self.partner_id:
-            _logger.warn("~ Haze: looping through self.invoice_line_ids")
+            _logger.warn("~ Haze: looping through self.invoice_line_ids %s " % self.invoice_line_ids)
             for line in self.invoice_line_ids:
                 _logger.warn("~ Haze: this is an insurance product: %s" % line.product_id.insurance)
                 
@@ -40,17 +40,27 @@ class AccountInvoice(models.Model):
                     partner_end = self.partner_id.date_end
                     
                     line.total_days = self.days_between(partner_start or insurance_start, partner_end or insurance_end)
-                    _logger.warn("~ Haze: total days was calculated to %s" % line.total_days)
-                    line.price = line.product_id.lst_price * line.total_days
+                    _logger.warn("~ Haze1: total days was calculated to %s" % line.total_days)
+                    line.price_unit = line.product_id.lst_price / 360 * line.total_days
+                    # ~ _logger.warn('Haze1 price %s' %line.price_unit)
                     line.price_subtotal = line.price_unit * line.quantity
-
+                    # ~ _logger.warn('Haze1 price sub %s' %line.price_subtotal)
+                    vals = { 'price_unit': line.price_unit,
+                             'price_subtotal': line.price_subtotal,
+                     }
+                    line.sudo().write(vals)
+                    _logger.warn('Haze1 vals %s' %vals)
+            # ~ self._onchange_eval('invoice_line_ids', "1", {})
         _logger.warn("~ Haze: _check_dates_insurance DONE")
 
 class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
     
-    @api.one
-    @api.onchange('product_id')
-    def _check_dates_insurance(self):
-        _logger.warn("~ Haze: in _check_dates_insurance 2 self.product_id %s" % self.product_id)
+   
+    @api.onchange('product_id','price_unit','price_subtotal')
+    
+    def _check_dates_insurance_line(self):
+        _logger.warn("~ Haze2: in _check_dates_insurance self.product_id%s" % self.product_id)
+        _logger.warn("~ Haze2: in _check_dates_insurance 2 self.product_id.price_unit %s" % self.product_id.lst_price)
+        # ~ _logger.warn("~ Haze: in _check_dates_insurance 2 self.product_id %s.price_subtotal" % self.product_)
         self.invoice_id._check_dates_insurance()
